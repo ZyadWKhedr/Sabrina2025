3fa55a6b4e0b8a9c60dbc7b85a8fa2461baec3f3
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-01-27 16:54:39 +0200

    navigate based on login state after splash screen is displayed using local storage

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -4,9 +4,12 @@ import 'package:get/get.dart';
 import 'package:sabrina2025/core/constants/app_colors.dart';
 import 'package:sabrina2025/core/constants/dimensions.dart';
 import 'package:sabrina2025/core/routes/app_routes.dart';
+import 'package:sabrina2025/services/local_storage_service.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+  SplashScreen({super.key});
+
+  final LocalStorageService _localStorageService = LocalStorageService();
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
@@ -16,9 +19,22 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(milliseconds: 3998), () {
-      Get.offNamed(AppRoutes.register);
-    });
+    _navigateBasedOnLoginState();
+  }
+
+  Future<void> _navigateBasedOnLoginState() async {
+    // Wait for the splash screen duration
+    await Future.delayed(const Duration(milliseconds: 3998));
+
+    // Check login state
+    final isLoggedIn = await widget._localStorageService.getLoginState();
+
+    // Navigate based on login state
+    if (isLoggedIn) {
+      Get.offNamed(AppRoutes.home); // Navigate to HomePage
+    } else {
+      Get.offNamed(AppRoutes.register); // Navigate to RegisterPage
+    }
   }
 
   @override
@@ -31,13 +47,11 @@ class _SplashScreenState extends State<SplashScreen> {
           Center(
             child: Image.asset(
               'assets/sabrina_logo.PNG',
-              width: Dimensions.width50 * 20, 
-              height: Dimensions.height20 * 10, 
+              width: Dimensions.width50 * 20,
+              height: Dimensions.height20 * 10,
             ),
           ),
-          SizedBox(
-              height: Dimensions
-                  .height20), 
+          SizedBox(height: Dimensions.height20),
 
           // Animated "SABRINA" Text
           AnimatedTextKit(
@@ -45,8 +59,7 @@ class _SplashScreenState extends State<SplashScreen> {
               FlickerAnimatedText(
                 'SABRINA',
                 textStyle: TextStyle(
-                  fontSize:
-                      Dimensions.font40, 
+                  fontSize: Dimensions.font40,
                   fontWeight: FontWeight.bold,
                   color: AppColors.mainColor,
                 ),
