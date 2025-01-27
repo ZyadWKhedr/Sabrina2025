490e2ab3e3fa0123d7ee1732bfcca7c718bfc1d0
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-01-27 18:12:07 +0200

    customize navigation bar and including font awesome icons

--- lib/core/app/custom_navigation_bar.dart
+++ lib/core/app/custom_navigation_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:sabrina2025/core/constants/app_colors.dart';
 import 'package:sabrina2025/core/constants/dimensions.dart';
@@ -19,116 +20,80 @@ class CustomNavigationBar extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
-    return Container(
-      margin: EdgeInsets.symmetric(
-          horizontal: screenWidth * 0.03, vertical: screenHeight * 0.05),
-      padding: EdgeInsets.symmetric(
-          horizontal: screenWidth * 0.03, vertical: screenHeight * 0.01),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(30),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.3),
-            spreadRadius: 2,
-            blurRadius: 4,
-            offset: const Offset(0, 2),
-          ),
-        ],
-      ),
-      child: GNav(
-        haptic: true,
-        tabBorderRadius: 20,
-        curve: Curves.easeInCirc,
-        duration: const Duration(milliseconds: 500),
-        gap: screenWidth * 0.002,
-        color: Colors.black,
-        activeColor: AppColors.mainColor,
-        iconSize: screenWidth * 0.06,
-        tabBackgroundColor: Colors.grey.withOpacity(12),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 40, left: 10, right: 10),
+      child: Container(
         padding: EdgeInsets.symmetric(
-            horizontal: screenWidth * 0.05, vertical: screenHeight * 0.007),
-        onTabChange: onTabChange,
-        selectedIndex: currentIndex,
-        tabs: [
-          GButton(
-            icon: Icons.circle,
-            iconColor: Colors.transparent,
-            leading: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.home_outlined,
-                    color: AppColors.iconColor, size: Dimensions.iconSize32),
-                SizedBox(height: screenHeight * 0.004),
-                Text(
-                  'Home',
-                  style: TextStyle(
-                    fontSize: screenWidth * 0.03,
-                    color: Colors.black,
-                    shadows: currentIndex == 0
-                        ? [
-                            BoxShadow(
-                                color: Colors.blue.withOpacity(0.6),
-                                blurRadius: 8)
-                          ]
-                        : null,
-                  ),
-                ),
-              ],
+            horizontal: screenWidth * 0.01, vertical: screenHeight * 0.015),
+        decoration: BoxDecoration(
+          color: AppColors.mainColor,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade300,
+              spreadRadius: 2,
+              blurRadius: 4,
+              offset: const Offset(0, 2),
             ),
-          ),
-          GButton(
-            icon: Icons.circle,
-            iconColor: Colors.transparent,
-            leading: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(CupertinoIcons.chat_bubble,
-                    color: Color(0xff003356), size: 24),
-                SizedBox(height: screenHeight * 0.004),
-                Text(
-                  'AI Chat',
-                  style: TextStyle(
-                      fontSize: screenWidth * 0.03, color: Colors.black),
-                ),
-              ],
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: GNav(
+            haptic: true,
+            tabBorderRadius: 20,
+            curve: Curves.easeInOut,
+            duration: const Duration(milliseconds: 300),
+            gap: screenWidth * 0.02,
+            color: Colors.black,
+            iconSize: screenWidth * 0.06,
+            tabBackgroundColor: Colors.white38,
+            padding: EdgeInsets.symmetric(
+              horizontal: screenWidth * 0.05,
+              vertical: screenHeight * 0.012,
             ),
-          ),
-          GButton(
-            icon: Icons.circle,
-            iconColor: Colors.transparent,
-            leading: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(CupertinoIcons.clock,
-                    color: Color(0xff003356), size: 24),
-                SizedBox(height: screenHeight * 0.004),
-                Text(
-                  'Reminders',
-                  style: TextStyle(
-                      fontSize: screenWidth * 0.03, color: Colors.black),
+            onTabChange: onTabChange,
+            selectedIndex: currentIndex,
+            tabs: [
+              GButton(
+                icon: FontAwesomeIcons.house,
+                text: 'Home',
+                iconColor: AppColors.iconColor,
+                textStyle: TextStyle(
+                  fontSize: Dimensions.font20 / 1.5,
+                  color: AppColors.iconColor,
                 ),
-              ],
-            ),
-          ),
-          GButton(
-            icon: Icons.circle,
-            iconColor: Colors.transparent,
-            leading: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(CupertinoIcons.heart,
-                    color: Color(0xff003356), size: 24),
-                SizedBox(height: screenHeight * 0.004),
-                Text(
-                  'Favourites',
-                  style: TextStyle(
-                      fontSize: screenWidth * 0.03, color: Colors.black),
+              ),
+              GButton(
+                icon: FontAwesomeIcons.cartShopping,
+                text: 'Cart',
+                iconColor: AppColors.iconColor,
+                textStyle: TextStyle(
+                  fontSize: Dimensions.font20 / 1.5,
+                  color: AppColors.iconColor,
                 ),
-              ],
-            ),
+              ),
+              GButton(
+                icon: FontAwesomeIcons.heart,
+                text: 'Favourites',
+                iconColor: AppColors.iconColor,
+                textStyle: TextStyle(
+                  fontSize: Dimensions.font20 / 1.5,
+                  color: AppColors.iconColor,
+                ),
+              ),
+              GButton(
+                icon: CupertinoIcons.person_fill,
+                text: 'Profile',
+                iconColor: AppColors.iconColor,
+                textStyle: TextStyle(
+                  fontSize: Dimensions.font20 / 1.5,
+                  color: AppColors.iconColor,
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -1,28 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:sabrina2025/core/app/custom_navigation_bar.dart';
 import 'package:sabrina2025/core/constants/app_colors.dart';
 import 'package:sabrina2025/core/constants/dimensions.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  // This will track the selected tab
+  final List<Widget> _pages = [
+    const HomeContent(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.search,
-                size: Dimensions.iconSize32,
-                color: AppColors.iconColor,
-              ),
-            ],
-          )
-        ],
-      ),
+      bottomNavigationBar: CustomNavigationBar(
+          currentIndex: _currentIndex,
+          onTabChange: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          }),
     );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  const HomeContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
